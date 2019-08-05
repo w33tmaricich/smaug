b61006a0c78a68e095420fd493e5a963d18067ad
Alexander Maricich <amaricich@skylinenet.net>
2019-08-05 16:59:43 -0400

    feat(core): storing recordings
    - Configure the application and what you want to record using a json file.
    - Updated dependency list.
    - You can record video and it will restart recordings at a given interval.
    
    TODO:
    - Monitor the number of files in the directory and remove old recordings.

--- project.clj
+++ project.clj
@@ -1,9 +1,14 @@
 (defproject smaug "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+  :description "Simple IP camera DVR"
+  :url "https://github.com/w33tmaricich/smaug"
+  :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[clj-time "0.15.1"]
+                 [org.clojure/core.async "0.4.500"]
+                 [org.clojure/data.json "0.2.6"]
+                 [org.clojure/tools.cli "0.4.2"]
+                 [w33t/kawa "0.1.2"]
+                 [org.clojure/clojure "1.10.0"]]
   :main ^:skip-aot smaug.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/smaug/core.clj
+++ src/smaug/core.clj
@@ -1,7 +1,125 @@
+; Launch the application:
+ ; kiki lein run
+
+; Run some tests:
+ ; kiki lein test
+
+; Build a jar (f):
+ ; kiki lein uberjar
+
+ ; kiki cat ./config.json | grep rtsp
+
+
 (ns smaug.core
+  (:require
+   [clojure.core.async :as a :refer [>! <! >!! <!! go chan buffer close! thread alts! alts!! timeout]]
+   [clojure.data.json :as json]
+   [clojure.tools.cli :refer [parse-opts]]
+   [kawa.core :refer [ffmpeg! ffplay!]]
+   [kawa.manager :as recording-manager]
+   )
   (:gen-class))
 
+; defaults
+(def config-file "./config.json")
+(def quit-chan (chan))
+
+; command line options
+(def cli-options
+  [["-c" "--config PATH"
+    "Path to json config file."
+    :default config-file]
+   ["-d" "--display" :default false]
+   ["-h" "--help"]])
+
+(defn- date []
+  (.format (java.text.SimpleDateFormat. "yyyy-MM-dd-hh-mm-SS") (java.util.Date.)))
+
+(defn- retrieve-configuration
+  "Reads in a json file at a given path. Checks that the json contains all
+  the information we need, and then returns it as a map."
+  [path]
+  (if true ; TODO: Add a check to ensure all required fields are present.
+    (let [file-contents (slurp path)
+          configuration (json/read-str file-contents :key-fn keyword)]
+      configuration)))
+
+(defn- record-camera
+  "Starts up a recording for a particular camera."
+  [id url storage-directory chunk-length-seconds]
+  (recording-manager/register (keyword id)
+                              (ffmpeg! :input-url url
+                                       :t chunk-length-seconds
+                                       :c:v "copy" :an
+                                       (str storage-directory id (date) ".mp4"))))
+
+(defn- recordings-start
+  "Spin up an ffmpeg process for each camera."
+  [{storage-directory :storage-directory
+    segments-minutes :segments-minutes
+    cameras :cameras}]
+  (println "Starting recording...")
+  (loop [camera (first cameras)
+         next-cameras (rest cameras)]
+    (let [{camera-name :name
+           camera-url :url} camera]
+      (println "------------")
+      (println camera-name)
+      (println camera-url)
+      (println (record-camera camera-name camera-url storage-directory (* 60000 segments-minutes)))
+      (if (not-empty next-cameras)
+        (recur (first next-cameras) (rest next-cameras)))))
+  (keys (recording-manager/ls))
+)
+
+(defn- recording-stop
+  "Stop a recording."
+  [id]
+  (recording-manager/kill (keyword id)))
+
+(def not-nil? (complement nil?))
+(defn- recordings-stop
+  "Stop a list of recordings."
+  [ids]
+  (loop [id (first ids)
+         id-list (rest ids)]
+    (if (not-nil? id)
+      (do
+       (recording-stop id)
+       (recur (first id-list)
+              (rest id-list))))))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Lets start some recordings!"
   [& args]
-  (println "Hello, World!"))
+  (let [options (:options (parse-opts args cli-options))
+        display? (-> options :display)
+        config (retrieve-configuration (-> options :config))
+        {storage-directory :storage-directory
+         segments-minutes :segments-minutes
+         max-segments :max-segments
+         cameras :cameras} config
+        ]
+
+    (println "Options:" options)
+    (println "Display?:" display?)
+    (println "Config:" config)
+    (println "storage-directory:" storage-directory)
+    (println "segments-minutes:" segments-minutes)
+    (println "max-segments:" max-segments)
+    (println "cameras:" cameras)
+    
+
+
+    (loop [recording? (recordings-start config)]
+      (Thread/sleep (* 6000 segments-minutes))
+      (println "Cameras currently recording:")
+      (println (keys (recording-manager/ls)))
+      (println "Stopping recordings...")
+      (recordings-stop (keys (recording-manager/ls)))
+      (println "Recordings stopped.")
+      (println "Restarting recordings...")
+      (recordings-start config)
+      (recur recording?))
+
+    ))
